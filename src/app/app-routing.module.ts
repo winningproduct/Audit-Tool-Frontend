b799d7ed1a608456648c253f0d0bbd3ec0ff237e
Joba <a.m.s.nirmal.a@gmail.com>
2020-01-07 15:57:07 +0530

    npm update , remove hash routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-{ path: 'audit', loadChildren: () => import('./audit/audit.module').then(m => m.AuditModule) },
-{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-{ path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) },
-{ path: '**', loadChildren: () => import('./audit/audit.module').then(m => m.AuditModule) },
+  {
+    path: 'audit',
+    loadChildren: () => import('./audit/audit.module').then(m => m.AuditModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'shared',
+    loadChildren: () =>
+      import('./shared/shared.module').then(m => m.SharedModule),
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./audit/audit.module').then(m => m.AuditModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/audit/audit-routing.module.ts
+++ src/app/audit/audit-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuditComponent } from './audit.component';
 import { ViewProductsComponent } from './containers/view-products/view-products.component';
 import { ViewPhasesComponent } from './containers/view-phases/view-phases.component';
 import { ViewQuestionsComponent } from './containers/view-questions/view-questions.component';
@@ -9,12 +8,15 @@ import { ViewQuestionsComponent } from './containers/view-questions/view-questio
 const routes: Routes = [
   { path: 'products', component: ViewProductsComponent },
   { path: 'products/:productId/phases', component: ViewPhasesComponent },
-  { path: 'products/:productId/phases/:productPhaseId', component: ViewQuestionsComponent },
-  { path: '**', component: ViewProductsComponent }
+  {
+    path: 'products/:productId/phases/:productPhaseId',
+    component: ViewQuestionsComponent,
+  },
+  { path: '**', component: ViewProductsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AuditRoutingModule {}
