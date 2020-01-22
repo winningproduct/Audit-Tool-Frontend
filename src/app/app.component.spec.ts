8f243f3fe38b091e5a9e92d851f1e0a936806633
nir099 <a.m.s.nirmal.a@gmail.com>
2020-01-22 12:32:43 +0530

    boiler plate to audit tol

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-boiler-plate'`, () => {
+  it(`should have as title 'auditTool'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-boiler-plate');
+    expect(app.title).toEqual('auditTool');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-boiler-plate!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to auditTool!');
   });
 });
