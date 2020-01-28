53188d005410714f257730a3e74ab1abb5801528
ChameeraD <chameeradulanga87@gmail.com>
2020-01-28 13:53:34 +0530

    Logout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
@@ -7,14 +8,13 @@ import { AuthService } from './services/auth/auth.service';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { AvatarModule } from 'ngx-avatar';
 
-
-
 @NgModule({
   declarations: [SharedComponent, NavBarComponent],
   imports: [
     CommonModule,
     SharedRoutingModule,
-    AvatarModule
+    AvatarModule,
+    BsDropdownModule.forRoot(),
   ],
   exports: [NavBarComponent],
   providers: [
