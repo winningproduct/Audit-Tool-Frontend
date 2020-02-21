56509acde33095ce898e86c6f96ad5bc6dfcbaae
ChameeraD <chameeradulanga87@gmail.com>
2020-02-21 16:57:24 +0530

    Role Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
-    canActivate : [AuthGuard,RoleGuard]
+    canActivate : [AuthGuard, RoleGuard]
   },
   {
     path: '**',
